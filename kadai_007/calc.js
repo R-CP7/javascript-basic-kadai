44a7267da34f521095df367c5faaae670a534d63
R-CP7 <138663910+R-CP7@users.noreply.github.com>
2023-08-18 00:23:33 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+
+if(3%3===0){
+  console.log('3倍です');
+  }
+  
+  if(5%5===0){
+    console.log('5倍です');
+    }
+    
+    if(3%3===0&5%5==0){
+      console.log('3と5倍です');
+      }
+
+      else{
+        console.log('num');
+      }
